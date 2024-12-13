6a5b8baab5b4dc934dabd9d43ce9f5ca7a43125e
MrBearPresident <jerrelatruwe@hotmail.com>
2024-12-13 21:31:05 +0100

    Get all the packages ready for instant testing

--- webpack.config.js
+++ webpack.config.js
@@ -18,12 +18,13 @@ module.exports = [
 
   {
     mode: 'development',
+    devtool: 'source-map',
     entry: {
       'bubble-card': './src/bubble-card.ts',
       'bubble-pop-up-fix': './src/bubble-pop-up-fix.ts'
     },
     output: {
-      path: path.resolve('/Volumes/config/www'),
+      path: path.resolve('//hercules/C - Docker/30-Home-Assistent/Config/www/community/Bubble-Card'),
       filename: '[name].js'
     }
   }
